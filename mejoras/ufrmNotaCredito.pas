6d5627128a18b00c757f95230983ea48b87c91fa
Dario Garcia <dario61081@gmail.com>
2019-04-24 23:24:33 -0400

    nota de venta

---
+++ mejoras/ufrmNotaCredito.pas
@@ -0,0 +1,24 @@
+unit ufrmNotaCredito;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, ufrmTemplateDatos, dr2gcomponentes;
+
+type
+  TfrmNotaCredito = class(TfrmDatos)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmNotaCredito: TfrmNotaCredito;
+
+implementation
+
+{$R *.dfm}
+
+end.
